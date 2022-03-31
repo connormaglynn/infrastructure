b277833edd7e6bcd2040f5cfc03b407a32c119c7
ConnorGlynn13 <connorglynn13@GMAIL.COM>
2022-03-31 19:18:46 +0100

    run formatter

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-    backend "remote" {
+  backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "connormaglynn"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-        name = "infrastructure"
+      name = "infrastructure"
     }
-    }
+  }
 }
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
-    triggers = {
+  triggers = {
     value = "A example resource that does nothing!"
-    }
+  }
 }
