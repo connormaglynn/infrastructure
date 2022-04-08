ec52894908940f84d5bed18811b01e35082b06ff
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2022-04-08 23:14:50 +0100

    add route53 for connorglynn.com (#2)

--- main.tf
+++ main.tf
@@ -8,11 +8,38 @@ terraform {
       name = "infrastructure"
     }
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "connorglynn.com"
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "connorglynn.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.connorglynn.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["connormaglynn.github.io"]
+}
+
+output "name_servers" {
+  value = aws_route53_zone.primary.name_servers
 }
