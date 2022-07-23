bf96d9a15cca2afa267d36b766da5d2ab36c4d54
connormaglynn <connor.glynn@digital.justice.gov.uk>
2022-07-23 23:09:30 +0100

    PP-30: ♻️ Seperate files

--- main.tf
+++ main.tf
@@ -15,39 +15,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
-resource "aws_route53_zone" "primary" {
-  name = "connorglynn.com"
-}
-
-resource "aws_route53_record" "main" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "connorglynn.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.connorglynn.com"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["connormaglynn.github.io"]
-}
-
-output "name_servers" {
-  value = aws_route53_zone.primary.name_servers
-}
-
-resource "aws_route53_record" "blog" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "blog.connorglynn.com"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["connormaglynn.github.io"]
-}
---
+++ resources.tf
@@ -0,0 +1,27 @@
+resource "aws_route53_zone" "primary" {
+  name = "connorglynn.com"
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "connorglynn.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.connorglynn.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["connormaglynn.github.io"]
+}
+
+resource "aws_route53_record" "blog" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "blog.connorglynn.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["connormaglynn.github.io"]
+}
