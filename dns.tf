efe827ad84349d6a9d04d687399d6ffc77ddc9cf
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2022-07-30 10:11:28 +0100

    PP-80: ✨ Add Google Site Verification Record to connorglynn.com (#6)

--- dns.tf
+++ dns.tf
@@ -10,6 +10,15 @@ resource "aws_route53_record" "main" {
   records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
 }
 
+resource "aws_route53_record" "google-site-verification" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "connorglynn.com"
+  type    = "TXT"
+  records = ["google-site-verification=Hih9PMtKjpHcKufTpVbknEdiUAIquqxMArSEvarxOJs"]
+  ttl     = "900"
+}
+
+
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.connorglynn.com"
