901c6618cc6ccd173dbae7beb052ba8bf3ffc5b5
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-03-06 23:50:28 +0000

    ✨Add simple web server 🆙 Update `aws` provider version (#8)

---
+++ terraform-up-and-running.tf
@@ -0,0 +1,31 @@
+# Basic web-server
+## security group for web server (allows web server to be public)
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance with a basic web server
+resource "aws_instance" "example" {
+  ami                    = "ami-006dcf34c09e50022"
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
