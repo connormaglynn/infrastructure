8d72d47d302b990952cdce8a916d5742b5b539c6
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-03-07 21:37:23 +0000

    ✨Add variable examples

--- terraform-up-and-running.tf
+++ terraform-up-and-running.tf
@@ -30,3 +30,50 @@
 #  }
 #}
 # -----------------------------------------------------------------------
+
+# Variables example
+variable "number_example" {
+  description = "An example of a number variable in Terraform"
+  type        = number
+  default     = 42
+}
+
+variable "list_example" {
+  description = "An example of a list in Terraform"
+  type = list
+  default = ["a","b","c"]
+}
+
+variable "list_numeric_example" {
+  description = "An example of a list with constraint in Terraform"
+  type = list(number)
+  default = [1,2,3]
+}
+
+variable "map_example" {
+  description = "An example of a map in Terraform"
+  type        = map(string)
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+    key3 = "value3"
+  }
+}
+
+variable "object_example" {
+  description = "An example of a structural type in Terraform"
+  type        = object({
+    name    = string
+    age     = number
+    tags    = list(string)
+    enabled = bool
+  })
+
+  default = {
+    name    = "value1"
+    age     = 42
+    tags    = ["a", "b", "c"]
+    enabled = "true"
+  }
+}
+# -----------------------------------------------------------------------
