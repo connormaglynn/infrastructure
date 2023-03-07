7645a60cc818875f7998c7cc4fdf8d75f0f0a132
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-03-07 23:06:00 +0000

    🔥 Remove server cluster

--- terraform-up-and-running.tf
+++ terraform-up-and-running.tf
@@ -80,154 +80,154 @@
 
 # Deploy Cluster of Servers
 ## variables for server cluster
-variable "server_port" {
-  description = "Port for servers"
-  type        = number
-  default     = 8080
-}
-
-## data sources for server cluster
-data "aws_vpc" "default" {
-  default = true
-}
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-## security group for web server (allows web server to be public)
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-## a launch configuration to be used by auto-scaling groupes
-resource "aws_launch_configuration" "server_cluster_example" {
-  image_id        = "ami-0aaa5410833273cfe"
-  instance_type   = "t2.nano"
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  # Required when using a launch configuration with an auto scaling group.
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-## definition of server cluster
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.server_cluster_example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 3
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-## Add application load balancer security group
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-## Add application load balancer
-resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-## Add application load balancer listener
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-## add a target group for load balancer
-resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-# Output
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
+#variable "server_port" {
+#  description = "Port for servers"
+#  type        = number
+#  default     = 8080
+#}
+#
+### data sources for server cluster
+#data "aws_vpc" "default" {
+#  default = true
+#}
+#data "aws_subnets" "default" {
+#  filter {
+#    name   = "vpc-id"
+#    values = [data.aws_vpc.default.id]
+#  }
+#}
+#
+### security group for web server (allows web server to be public)
+#resource "aws_security_group" "instance" {
+#  name = "terraform-example-instance"
+#
+#  ingress {
+#    from_port   = 8080
+#    to_port     = 8080
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
+#
+### a launch configuration to be used by auto-scaling groupes
+#resource "aws_launch_configuration" "server_cluster_example" {
+#  image_id        = "ami-0aaa5410833273cfe"
+#  instance_type   = "t2.nano"
+#  security_groups = [aws_security_group.instance.id]
+#
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World" > index.html
+#              nohup busybox httpd -f -p ${var.server_port} &
+#              EOF
+#
+#  # Required when using a launch configuration with an auto scaling group.
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
+#
+### definition of server cluster
+#resource "aws_autoscaling_group" "example" {
+#  launch_configuration = aws_launch_configuration.server_cluster_example.name
+#  vpc_zone_identifier  = data.aws_subnets.default.ids
+#
+#  target_group_arns = [aws_lb_target_group.asg.arn]
+#  health_check_type = "ELB"
+#
+#  min_size = 2
+#  max_size = 3
+#
+#  tag {
+#    key                 = "Name"
+#    value               = "terraform-asg-example"
+#    propagate_at_launch = true
+#  }
+#}
+#
+### Add application load balancer security group
+#resource "aws_security_group" "alb" {
+#  name = "terraform-example-alb"
+#
+#  # Allow inbound HTTP requests
+#  ingress {
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  # Allow all outbound requests
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
+#
+### Add application load balancer
+#resource "aws_lb" "example" {
+#  name               = "terraform-asg-example"
+#  load_balancer_type = "application"
+#  subnets            = data.aws_subnets.default.ids
+#  security_groups    = [aws_security_group.alb.id]
+#}
+#
+### Add application load balancer listener
+#resource "aws_lb_listener" "http" {
+#  load_balancer_arn = aws_lb.example.arn
+#  port              = 80
+#  protocol          = "HTTP"
+#
+#  # By default, return a simple 404 page
+#  default_action {
+#    type = "fixed-response"
+#
+#    fixed_response {
+#      content_type = "text/plain"
+#      message_body = "404: page not found"
+#      status_code  = 404
+#    }
+#  }
+#}
+#resource "aws_lb_listener_rule" "asg" {
+#  listener_arn = aws_lb_listener.http.arn
+#  priority     = 100
+#
+#  condition {
+#    path_pattern {
+#      values = ["*"]
+#    }
+#  }
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.asg.arn
+#  }
+#}
+#
+### add a target group for load balancer
+#resource "aws_lb_target_group" "asg" {
+#  name     = "terraform-asg-example"
+#  port     = var.server_port
+#  protocol = "HTTP"
+#  vpc_id   = data.aws_vpc.default.id
+#
+#  health_check {
+#    path                = "/"
+#    protocol            = "HTTP"
+#    matcher             = "200"
+#    interval            = 15
+#    timeout             = 3
+#    healthy_threshold   = 2
+#    unhealthy_threshold = 2
+#  }
+#}
+#
+## Output
+#output "alb_dns_name" {
+#  value       = aws_lb.example.dns_name
+#  description = "The domain name of the load balancer"
+#}
 # -----------------------------------------------------------------------
