6a770e6024be6d856b5462bf0680b74f9f640bcb
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-07-12 22:13:42 +0100

    ♻️Refactor to use default workspace

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "connormaglynn"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "infrastructure"
-    }
   }
   required_providers {
     aws = {
