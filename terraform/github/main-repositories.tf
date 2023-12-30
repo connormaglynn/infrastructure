63678e31fd4c35d341de7e63439e5c84f94d8cea
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-12-30 15:24:19 +0000

    ✨ import `infrastructure` repository (#32)

--- terraform/github/main-repositories.tf
+++ terraform/github/main-repositories.tf
@@ -9,3 +9,20 @@ resource "github_repository" "example_of_terraform_managed" {
   auto_init                 = true
   delete_branch_on_merge    = true
 }
+
+import {
+  id = "infrastructure"
+  to = github_repository.infrastructure
+}
+
+resource "github_repository" "infrastructure" {
+  name                      = "infrastructure"
+  description               = "This repository is managed in Terraform"
+  visibility                = "public"
+  allow_merge_commit        = false
+  allow_squash_merge        = true
+  allow_rebase_merge        = false
+  squash_merge_commit_title = "PR_TITLE"
+  auto_init                 = true
+  delete_branch_on_merge    = true
+}
