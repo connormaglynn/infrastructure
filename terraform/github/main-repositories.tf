b45d8bccc91587f27a073f1705a85d7f315bb076
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-12-30 20:54:14 +0000

    ♻️ Refactor module into variables

--- terraform/github/main-repositories.tf
+++ terraform/github/main-repositories.tf
@@ -24,17 +24,20 @@ resource "github_repository" "infrastructure" {
 
 import {
   id = "scripts"
-  to = module.repositories.github_repository.repositories["scripts"]
+  to = github_repository.repositories["scripts"]
 }
 
-module "repositories" {
-  source = "./modules/repositories"
-  repositories = [
-    {
-      name = "scripts"
-    }
-  ]
-  providers = {
-    github = github
+resource "github_repository" "repositories" {
+  for_each = {
+    for index, repository in var.repositories : repository.name => repository
   }
+  name                      = each.value.name
+  description               = each.value.description
+  visibility                = each.value.visibility
+  allow_merge_commit        = false
+  allow_squash_merge        = true
+  allow_rebase_merge        = false
+  squash_merge_commit_title = "PR_TITLE"
+  auto_init                 = true
+  delete_branch_on_merge    = true
 }
---
+++ terraform/github/variables.tf
@@ -0,0 +1,13 @@
+variable "PAT_GITHUB_API_TOKEN" {
+  type      = string
+  sensitive = true
+}
+
+variable "repositories" {
+  description = "All repositories managed in terraform"
+  type = list(object({
+    name        = string
+    description = optional(string, "This repository is managed in Terraform")
+    visibility  = optional(string, "public")
+  }))
+}
